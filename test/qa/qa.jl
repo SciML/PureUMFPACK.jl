cd191d04bd9ebb1e11af5c7d73b5a197dac6e0e2
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-07-10 16:21:24 -0400

    Use SciMLTesting API docs QA
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- test/qa/qa.jl
+++ test/qa/qa.jl
@@ -1,10 +1,9 @@
-using SciMLTesting, PureUMFPACK, Test
-
-include("public_api_docs.jl")
+using SciMLTesting, PureUMFPACK
 
 run_qa(
     PureUMFPACK;
     explicit_imports = true,
+    api_docs_kwargs = (; rendered = true),
     ei_kwargs = (;
         # `Base.OneTo` (gplu.jl) is not declared public in Base.
         all_qualified_accesses_are_public = (; ignore = (:OneTo,)),
